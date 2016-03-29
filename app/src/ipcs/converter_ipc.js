82675843a8576878ea68a0090466be78bfb451a5
Chamnap Chhorn <chamnapchhorn@gmail.com>
2016-03-29 19:53:57 +0700

    update prn2xls

--- app/src/ipcs/converter_ipc.js
+++ app/src/ipcs/converter_ipc.js
@@ -3,7 +3,7 @@ const dialog  = require('electron').dialog;
 const prn2xls = require('prn2xls');
 
 ipcMain.on('converter.choose_source_file.request', function(event, args) {
-  var path = dialog.showOpenDialog({ properties: [ 'openFile' ], filters: [{ name: 'PRN File', extensions: ['PRN'] }] });
+  var path = dialog.showOpenDialog({ properties: [ 'openFile' ], filters: [{ name: 'PRN File', extensions: ['PRN', 'prn'] }] });
 
   event.sender.send('converter.choose_source_file.response', path);
 });
